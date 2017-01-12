1810aec53a536b2d51757b5ce14dd1528ba993f1
isurunix <isurunix@gmail.com>
2017-01-12 10:24:45 +0530

    Added DBConnector and new packages

---
+++ src/main/java/lk/grp/synergy/db/DBConnector.java
@@ -0,0 +1,34 @@
+package lk.grp.synergy.db;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * Created by isuru on 1/12/17.
+ *
+ * This class provides DBConnector with DB pooling
+ * Use connections for every database connection used within the application
+ */
+public class DBConnector {
+
+    public static Connection getConnection() throws NamingException, SQLException {
+        InitialContext initialContext = new InitialContext();
+        Context context = (Context) initialContext.lookup("java:comp/env");
+        //The JDBC Data source that we just created
+        DataSource ds = (DataSource) context.lookup("connpool");
+        Connection connection = ds.getConnection();
+
+        if (connection == null)
+        {
+            throw new SQLException("Error establishing connection!");
+        }
+
+        return connection;
+    }
+
+}
+
