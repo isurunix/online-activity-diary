a90ea0d1f2735dce00a550046cd8da2416cefcee
isurunix <isurunix@gmail.com>
2017-01-13 22:32:09 +0530

    Added CORS support

---
+++ src/main/java/lk/grp/synergy/service/CORSFilter.java
@@ -0,0 +1,27 @@
+package lk.grp.synergy.service;
+
+/**
+ * Created by isuru on 1/13/17.
+ */
+import javax.inject.Singleton;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+
+@Singleton
+@Provider
+public class CORSFilter implements ContainerResponseFilter {
+
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
+            throws IOException {
+
+        MultivaluedMap<String, Object> headers = responseContext.getHeaders();
+
+        headers.add("Access-Control-Allow-Origin", "*");
+        headers.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+        headers.add("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, X-Codingpedia");
+    }
+}
