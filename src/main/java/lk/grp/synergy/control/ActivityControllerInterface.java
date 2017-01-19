f5250aa1bc71076d6da6576bd420b02de5920c94
isurunix <isurunix@gmail.com>
2017-01-19 19:45:25 +0530

    removed ide configs and added new classes

---
+++ src/main/java/lk/grp/synergy/control/ActivityControllerInterface.java
@@ -0,0 +1,7 @@
+package lk.grp.synergy.control;
+
+/**
+ * Created by isuru on 1/19/17.
+ */
+public interface ActivityControllerInterface {
+}
---
+++ src/main/java/lk/grp/synergy/control/NotificationControllerInterface.java
@@ -0,0 +1,7 @@
+package lk.grp.synergy.control.impl;
+
+/**
+ * Created by isuru on 1/19/17.
+ */
+public interface NotificationControllerInterface {
+}
