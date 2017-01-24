cc3af72550aed53901ca54abd1854f989f8f5c74
knsandu <klsanduninimesha@gmail.com>
2017-01-24 13:53:03 +0530

    Create the facultyController class

---
+++ src/main/java/lk/grp/synergy/control/impl/FacultyController.java
@@ -0,0 +1,25 @@
+package lk.grp.synergy.control.impl;
+
+import lk.grp.synergy.control.FacultyControllerInterface;
+import lk.grp.synergy.model.Faculty;
+
+/**
+ * Created by USER on 1/24/2017.
+ */
+public class FacultyController implements FacultyControllerInterface{
+
+    @Override
+    public void addFaculty(Faculty faculty) {
+        
+    }
+
+    @Override
+    public void getAllFaculties() {
+
+    }
+
+    @Override
+    public void getFaculty(int facId) {
+
+    }
+}
