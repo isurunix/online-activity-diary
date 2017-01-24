afadfd85904851a7b0a93244fd4343a94473f16c
knsandu <klsanduninimesha@gmail.com>
2017-01-24 13:50:08 +0530

    Create the notificationController class

---
+++ src/main/java/lk/grp/synergy/control/impl/NotificationController.java
@@ -0,0 +1,23 @@
+package lk.grp.synergy.control.impl;
+
+import lk.grp.synergy.control.NotificationControllerInterface;
+import lk.grp.synergy.model.Notification;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * Created by USER on 1/24/2017.
+ */
+public class NotificationController implements NotificationControllerInterface {
+
+    @Override
+    public List<Notification> getAllPendingNotifications() {
+        return null;
+    }
+
+    @Override
+    public List<Notification> getAllNotificationsPerHour(LocalDateTime time) {
+        return null;
+    }
+}
